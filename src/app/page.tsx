f7b76dfb1e74f0d25cd8dce803bccd92525407a7
Jun Polo <junpolojr@gmail.com>
2025-07-10 14:50:31 +0800

    feat(components): add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+"use client";
 import React from "react";
+
+import { Button } from "@components";
 
 export default function HomePage() {
   return (
-    <div className="grid min-h-screen items-center justify-center">
-      <p>Hello World</p>
-    </div>
+    <section className="grid min-h-screen items-center justify-center">
+      <div>
+        <p>Hello World</p>
+        <Button onClick={() => console.log("test")}>Button</Button>
+      </div>
+    </section>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React, { PropsWithChildren } from "react";
+
+type ButtonProps = Required<PropsWithChildren> & {
+  className?: string | undefined;
+  onClick?: () => void;
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  className,
+}) => {
+  const handleClick = () => {
+    if (onClick) onClick();
+  };
+
+  return (
+    <button
+      className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer ${className}`}
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  );
+};

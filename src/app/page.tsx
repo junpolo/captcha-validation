cdeb811737631fa229c0069214413a1deb80f89c
Jun Polo <junpolojr@gmail.com>
2025-07-10 14:36:33 +0800

    ci: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div className="grid min-h-screen items-center justify-center">
+      <p>Hello World</p>
+    </div>
+  );
+}
